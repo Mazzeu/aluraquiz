c9643cc5a8a4e9444e904ad6de3ff6c1c0a5135d
Mazzeu <juliomazzeo@gmail.com>
2021-01-27 11:02:04 -0300

    codigo Aula1 Alurajs

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
+import Head from 'next/head';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -24,8 +27,14 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno do useState', name, setName);
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+      <title>Greed Island</title>
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Header>
@@ -33,6 +42,22 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento){
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissao por meio do react');
+            }}>
+              <input 
+                onChange={function (infosDoEvento) {
+                console.log(infosDoEvento.target.value);
+                // name = infosDoEvento.target.value;
+                setName(infosDoEvento.target.value);
+              }} 
+                placeholder="Qual seu nome usuario de Nen?"/>
+              <button type="submit" disabled={name.length === 0}>
+                Jogar como {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
